f09eb7800e220d6d85f01e4e92b03980a8d15032
Joe Timlin <jhthx9@mail.missouri.edu>
2018-06-12 13:14:39 -0500

    implemented expenses

---
+++ Expenses/Expense.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Expenses
+//
+//  Created by Joe Timlin on 6/12/18.
+//  Copyright © 2018 Joe Timlin. All rights reserved.
+//
+
+import Foundation
+
+struct Expense {
+    let title: String
+    let amount: Double
+    let date: Date
+}
